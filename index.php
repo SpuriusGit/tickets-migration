d3ace25eb524a613efb35454cb6d61e6be6e4a03
Spurius <d.tatochenko@gmail.com>
2025-10-29 09:44:43 +0200

    🩹 move index.php to main project folder - now we can just run "php index.php"

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-require __DIR__ . '/Ticket.php';
-require __DIR__ . '/TicketFetcher.php';
-require __DIR__ . '/CsvExporter.php';
+require __DIR__ . '/src/Ticket.php';
+require __DIR__ . '/src/TicketFetcher.php';
+require __DIR__ . '/src/CsvExporter.php';
 
 $subdomain = 'nocompany-68451';
 $email = 'd.tatochenko@gmail.com';
@@ -13,6 +13,6 @@ $apiToken = '62ZRFqYFFeOGvn7ZVYBEwLgBTy9kgf7bu2WoDvNm';
 $fetcher = new TicketFetcher($subdomain, $email, $apiToken);
 $tickets = $fetcher->fetchTickets();
 
-CsvExporter::export(__DIR__ . '/exports/tickets.csv', $tickets);
+CsvExporter::export(__DIR__ . '/src/exports/tickets.csv', $tickets);
 
 echo "Tickets exported: " . count($tickets) . "\n";
